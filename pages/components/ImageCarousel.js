2944e1c436980b430d8ca81c7a5cfe5fe2c7ae49
jkinney24 <joekinney53@gmail.com>
2023-05-19 18:35:05 -0500

    added some images, need to add more

--- pages/components/ImageCarousel.js
+++ pages/components/ImageCarousel.js
@@ -12,15 +12,21 @@ function ImageCarousel() {
             transitionTime={2000} // Adjust this to slow down the transition
         >
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
+                <img src="/Washington.jpg" alt="George Washington" />
+                <img src="/JoanOfArc.png" alt="Joan of Arc" />
+                <img src="/williamShake.jpg" alt="William Shakespeare" />
                 {/* ...more images */}
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
-                <img src="/alexTheGreat.jpg" alt="Alexander the Great" />
+                <img src="/AlexTheGreat.jpg" alt="Alexander the Great" />
+                <img src="/Socrates.jpg" alt="Alexander the Great" />
+                <img src="/GenghisKhan.jpg" alt="Alexander the Great" />
+                {/* ...more images */}
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <img src="/AlexTheGreat.jpg" alt="Alexander the Great" />
+                <img src="/Socrates.jpg" alt="Alexander the Great" />
+                <img src="/GenghisKhan.jpg" alt="Alexander the Great" />
                 {/* ...more images */}
             </div>
             {/* ...more divs */}
